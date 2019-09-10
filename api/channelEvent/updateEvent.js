01a35c4c92f30f440a336b247704a06f30f651e5
choi jinwoo <chlwlsdn0828@gmail.com>
2019-09-10 12:01:12 +0900

    [FIX,ADD] updateEvent, updateChannel
    이벤트 업데이트 형식
     -(validation삭제/querystring으로
      event_id받음)
    채널 업데이트 추가

--- api/channelEvent/updateEvent.js
+++ api/channelEvent/updateEvent.js
@@ -1,36 +1,32 @@
 const models = require("../../models/models");
 const colorConsole = require("../../lib/console");
-const Validation = require("../../lib/validation");
 const isMember = require("../channelEvent/isMember");
 const getChannelId = require("../channelEvent/getChannelId");
 
 module.exports = async function (req, res) {
     colorConsole.green("[channelEvent] 일정 변경");
     const user = req.user;
+    const { event_id } = req.query; //querystirng(event_id : update event id)
     const { body } = req;
     body.author = user.user_id;
     
     colorConsole.gray("request");
     colorConsole.gray({ body });
 
-    try {
-        await Validation.validateUpdateEvent(body);
-    } catch(err) {
-        colorConsole.yellow(err.message);
-        return res.status(400).json({ status : 400, message : "검증 오류입니다." });
+    if (!event_id) {
+        colorConsole.yellow("검증 오류입니다.");
+		return res.status(400).json({ status : 400, message : "검증 오류입니다." });
     }
-    
+
     try {
-        const channelId = await getChannelId(body.id);
+        const channelId = await getChannelId(event_id);
         
         if (!await isMember(user.user_id, channelId)) {
             colorConsole.yellow("[channelEvent] 일정 변경 권한이 없습니다.");
             return res.status(403).json({ status : 403, message : "일정 변경 권한이 없습니다." });
         }
         
-        const { id, channel_id, title, start_date, end_date, author } = body;
-        await models.ChannelEvent.update({ id, channel_id, title, start_date, end_date, author }, { where : { id : body.id } });
-
+        await models.ChannelEvent.update( body, { where : { id : event_id } });
         return res.status(200).json({ status : 200, message : "일정 변경에 성공하였습니다." });
     } catch(err) {
         colorConsole.red(err.message);
--- router/channelAdmin.js
+++ router/channelAdmin.js
@@ -4,10 +4,12 @@ const createChannel = require("../api/channelAdmin/createChannel");
 const awaitUser = require("../api/channelAdmin/awaitUser");
 const allowuser = require("../api/channelAdmin/allowUser");
 const rejectUser = require("../api/channelAdmin/rejectUser");
+const updateChannel = require("../api/channelAdmin/updateChannel");
 const router = express.Router();
 
 router.get("/", tokenMiddleware, createChannel);
 router.get("/await-user", tokenMiddleware, awaitUser);
 router.get("/allow", tokenMiddleware, allowuser);
 router.delete("/reject", tokenMiddleware, rejectUser);
+router.put("/update", tokenMiddleware, updateChannel);
 module.exports = router;
