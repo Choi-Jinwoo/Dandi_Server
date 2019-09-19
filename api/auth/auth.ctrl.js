88a2aadf262304b2cc2f24d0461fbd93cbc87703
choi jinwoo <chlwlsdn0828@gmail.com>
2019-09-19 12:14:08 +0900

    [FIX] isOverlapped data
    중복확인 데이터 제거

--- api/auth/auth.ctrl.js
+++ api/auth/auth.ctrl.js
@@ -22,10 +22,10 @@ exports.isOverlapped = async (req, res) => {
 
 		if (overlapUser) {
 			colorConsole.yellow('[auth] 중복된 아이디입니다.');
-			return res.status(400).json({ status : 400, message : '중복된 아이디입니다.', data : { isOverlapped : true } });     
+			return res.status(400).json({ status : 400, message : '중복된 아이디입니다.' });     
 		}
 		
-		return res.status(200).json({ status : 200, message : '중복되지 않은 아이디입니다.', data : { isOverlapped : false } });
+		return res.status(200).json({ status : 200, message : '중복되지 않은 아이디입니다.' });
 	} catch(err) {
 		colorConsole.red(err.message);
 		return res.status(500).json({ status : 500, message : '중복확인에 실패하였습니다.' });
