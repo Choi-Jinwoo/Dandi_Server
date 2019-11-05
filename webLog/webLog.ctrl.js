94b6de2c7c27533ee9c2cb99da1bfa76eb60eeb7
choi jinwoo <chlwlsdn0828@gmail.com>
2019-11-05 10:15:42 +0900

    [FIX] weblog modify

--- webLog/webLog.ctrl.js
+++ webLog/webLog.ctrl.js
@@ -9,7 +9,9 @@ exports.getLog = async (req, res) => {
     month = month < 10 ? `0${month}` : month;
     let date = today.getDate();
     date = date < 10 ? `0${date}` : date;
-    const data = fs.readFileSync(`./logs/Date/Log_${year}-${month}-${date}.log`).toString().replace(/info:/gi, '</br>info:');
+    let data = fs.readFileSync(`./logs/Date/Log_${year}-${month}-${date}.log`).toString().replace(/info:/gi, '</br>info:');
+    data = data.split('info:');
+    data.reverse();
     return res.status(200).send(`성공적으로 로그를 조회하였습니다.</br>로그 요청 시각: ${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()} </br></br><h3>----- START LOG FILE -----</br></h3>${data}</br></br><h3>----- END LOG FILE -----</h3>`);
   } catch (err) {
     colorConsole.red(err.message);
